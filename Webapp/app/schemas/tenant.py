4c381cd8b5cff7dbcfc8bf3a225eed01e3d50a61
Adediran <adesalawu@outlook.com>
2025-03-04 20:03:32 +0100

    modified AWS Network

--- Webapp/app/schemas/tenant.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class TenantCreate(BaseModel):
-    domain: str
-    admin_email: str
-    sip_trunk_provider: str
--- aws/modules/network/output.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-
-output "public_subnet_1_id" {
-  value = var.SBCSWE_HFE_SUBNET
-}
-
